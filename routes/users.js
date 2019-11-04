ac20e9148c969d0cbb157191312f0e1c4f06966d
Steven Zilberberg <srz222@gmail.com>
2019-11-03 23:14:02 -0500

    Added login logic and token generation

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const User = require('../models/user');
@@ -45,6 +46,45 @@ router.post('/signup', (req, res, next) => {
             }
         })
         .catch();    
+});
+
+// Log in
+router.post('/login', (req, res, next) => {
+     User.find({email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1){
+                return res.status(401).json({
+                    message: 'Invalid Credentials'
+                });
+            } else {
+                bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                    if (err) {
+                        return res.status(401).json({
+                            message: 'Invalid Credentials'
+                        });
+                    }
+
+                    if (result){
+                        const token = jwt.sign({ email: user[0].email, userId: user[0]._id }, "secret", { expiresIn: '1h'});
+                        return res.status(200).json({
+                            message: 'Access Granted',
+                            token: token
+                        });
+                    }
+
+                    res.status(401).json({
+                        message: 'Invalid Credentials'
+                    });
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.delete('/:userId', (req, res, next) => {
